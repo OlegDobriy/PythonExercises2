cd1373bdc5373e118fe98ff4fd989e8815ede45d
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-04-04 23:06:44 +0300

    Проверка валидности фикстуры и актуальности сессии

--- conftest.py
+++ conftest.py
@@ -1,17 +1,27 @@
-from model.contact import Contact
 from fixture.application import Application
 import pytest
 
 
-@pytest.fixture(scope='session')
-# scope указывает, что браузер запускается один раз на всю сессию,
-# а не каждый раз заново
-def app(request):
-    fixture = Application()
-    fixture.session.login(username="Admin", password="secret")
+fixture = None
+
+
+@pytest.fixture
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.check_login(username="Admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope='session', autouse=True)
+def app_stop(request):
 
     def final():
-        fixture.session.logout()
+        fixture.session.check_logout()
         fixture.destroy()
 
     request.addfinalizer(final)
