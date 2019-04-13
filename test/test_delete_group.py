933df60cfab4d3e1011fc60c35418da78988ba33
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-04-13 14:36:11 +0300

    Добавлена проверка на равенство записей групп

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -2,9 +2,11 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
-    old_groups_count = app.group.get_groups_list()
+    old_groups_list = app.group.get_groups_list()
     if app.group.count() == 0:
         app.group.create(Group(name='GroupName_delete_first_group'))
     app.group.delete_first_group()
-    new_groups_count = app.group.get_groups_list()
-    assert len(old_groups_count) - 1 == len(new_groups_count)
+    new_groups_list = app.group.get_groups_list()
+    assert len(old_groups_list) - 1 == len(new_groups_list)
+    old_groups_list[0:1] = []
+    assert old_groups_list == new_groups_list
