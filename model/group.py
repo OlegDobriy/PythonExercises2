5aa2dd8e84b9d5173ff599add9afd88b88438ed7
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-04-22 23:01:08 +0300

    Параметризация создания групп

--- model/group.py
+++ model/group.py
@@ -10,7 +10,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return '{%s:%s}' % (self.id, self.name)
+        return '{%s:%s:%s:%s}' % (self.id, self.name, self.header, self.footer)
 
     def __eq__(self, other):  # сравнивать по id и name, а не по расположению в памяти
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_sting(prefix, max_len):
+    symbols = string.ascii_letters + string.digits
+    return prefix + (''.join([random.choice(symbols) for i in range(random.randrange(max_len))]))
+
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ['', random_sting('name', 5)]
+    for header in ['', random_sting('header', 10)]
+    for footer in ['', random_sting('footer', 15)]
+]
+
+
+@pytest.mark.parametrize('group_data', testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group_data):
     old_groups_list = app.group.get_groups_list()
-    new_group = Group(name="GroupName", header="GroupHeader", footer="GroupFooter")
-    app.group.create(new_group)
+    app.group.create(group_data)
     assert len(old_groups_list) + 1 == app.group.count()  # справа берется только количество, а не весь список
     new_groups_list = app.group.get_groups_list()  # чтобы облегчить тест. Если прошел, то далее сравнить списки
-    old_groups_list.append(new_group)
+    old_groups_list.append(group_data)
     assert sorted(old_groups_list, key=Group.sorting_id_or_maxsize) == sorted(new_groups_list,
                                                                               key=Group.sorting_id_or_maxsize)
-
-
-def test_add_empty_group(app):
-    old_groups_list = app.group.get_groups_list()
-    new_group = Group(name="", header="", footer="")
-    app.group.create(new_group)
-    assert len(old_groups_list) + 1 == app.group.count()
-    new_groups_list = app.group.get_groups_list()
-    old_groups_list.append(new_group)
-    assert sorted(old_groups_list, key=Group.sorting_id_or_maxsize) == sorted(new_groups_list, key=Group.sorting_id_or_maxsize)
