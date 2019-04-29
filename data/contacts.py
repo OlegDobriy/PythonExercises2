fe5f315520db1732e8bae617df2826db9fc3e712
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-04-29 21:33:25 +0300

    Revert "Статичные тестовые данные для групп и контактов"
    This reverts commit a0391a53c5c38f278818b98169f345a0aa22807b.

--- data/contacts.py
+++
@@ -1,8 +0,0 @@
-from model.contact import Contact
-
-testdata = [
-    Contact(firstname='firstname_1', lastname='lastname_1', address='address_1', homephone='homephone_1',
-            email3='email3_1'),
-    Contact(firstname='firstname_2', lastname='lastname_2', address='address_2', homephone='homephone_2',
-            email3='email3_2')
-]
