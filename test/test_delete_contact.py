7161ce04364e3ae607823b439c4ca2c11ddc5749
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-04 20:09:08 +0300

    Изменение теста на удаление контакта

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,29 +1,15 @@
 from model.contact import Contact
-from random import randrange
-
-
-def test_delete_first_contact(app, db, check_ui):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname='FirstName_delete_first_contact'))
-    old_contacts_list = db.get_contacts_list()
-    app.contact.delete_first_contact()
-    new_contacts_list = db.get_contacts_list()
-    old_contacts_list[0:1] = []
-    assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) == sorted(old_contacts_list,
-                                                                                  key=Contact.sorting_id_or_maxsize)
-    if check_ui:
-        assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) ==\
-               sorted(db.get_contacts_list(), key=Contact.sorting_id_or_maxsize)
+import random
 
 
 def test_delete_random_contact(app, db, check_ui):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname='FirstName_delete_first_contact'))
     old_contacts_list = db.get_contacts_list()
-    index = randrange(len(old_contacts_list))
-    app.contact.delete_contact_by_index(index)
+    contact = random.choice(old_contacts_list)
+    app.contact.delete_contact_by_id(contact.id)
     new_contacts_list = db.get_contacts_list()
-    old_contacts_list[index:index+1] = []  # вырезать кусок от index до index+1
+    old_contacts_list.remove(contact)  # вырезать кусок от index до index+1
     assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) == sorted(old_contacts_list,
                                                                                   key=Contact.sorting_id_or_maxsize)
     if check_ui:
