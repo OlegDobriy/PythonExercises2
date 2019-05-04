23a19572ccf88ca3cca98688d5c888ab891b509f
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-04 20:21:16 +0300

    Тесты сравнения списка групп и контактов в ui и БД

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,10 +1,20 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
     ui_list = app.group.get_groups_list()
 
     def clean(group):
-        return Group(id=group.id, name=group.name.strip())
+        return Group(id=group.id, name=group.name.strip())  # убрать лишние пробелы
     db_list = map(clean, db.get_groups_list())
     assert sorted(ui_list, key=Group.sorting_id_or_maxsize) == sorted(db_list, key=Group.sorting_id_or_maxsize)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contacts_list()
+
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    db_list = map(clean, db.get_contacts_list())
+    assert sorted(ui_list, key=Contact.sorting_id_or_maxsize) == sorted(db_list, key=Contact.sorting_id_or_maxsize)
