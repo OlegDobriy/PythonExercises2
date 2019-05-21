8f251f61af5e2c3d5fcd7a2d3994904911c97e3d
OlegDobriy <46223141+OlegDobriy@users.noreply.github.com>
2019-05-21 23:01:09 +0300

    Изменен тест модификации контакта

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,20 +1,35 @@
 from model.contact import Contact
-from random import randrange
+import random
 
+
+# def test_modify_contact(app, db, check_ui, json_contacts):
+#     new_contact = json_contacts
+#     if len(db.get_contacts_list()) == 0:
+#         app.contact.create(Contact(firstname='firstname_test_modify'))
+#     old_contacts_list = sorted(db.get_contacts_list(), key=Contact.sorting_name)
+#     index = randrange(len(old_contacts_list))
+#     new_contact.id = old_contacts_list[index].id  # получить id первого контакта
+#     new_contact.lastname = old_contacts_list[index].lastname  # получить lastname первого контакта
+#     app.contact.modify_contact_by_index(index, new_contact)
+#     new_contacts_list = db.get_contacts_list()
+#     old_contacts_list[index] = new_contact
+#     assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) ==\
+#         sorted(old_contacts_list, key=Contact.sorting_id_or_maxsize)
+#     if check_ui:
+#         assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) ==\
+#             sorted(app.contact.get_contacts_list(), key=Contact.sorting_id_or_maxsize)
 
 def test_modify_contact(app, db, check_ui, json_contacts):
     new_contact = json_contacts
     if len(db.get_contacts_list()) == 0:
         app.contact.create(Contact(firstname='firstname_test_modify'))
-    old_contacts_list = sorted(db.get_contacts_list(), key=Contact.sorting_name)
-    index = randrange(len(old_contacts_list))
-    new_contact.id = old_contacts_list[index].id  # получить id первого контакта
-    new_contact.lastname = old_contacts_list[index].lastname  # получить lastname первого контакта
-    app.contact.modify_contact_by_index(index, new_contact)
+    old_contacts_list = db.get_contacts_list()
+    contact = random.choice(old_contacts_list)
+    index = int(old_contacts_list.index(contact))
+    new_contact.id = contact.id
+    app.contact.modify_contact_by_id(contact.id, new_contact)
     new_contacts_list = db.get_contacts_list()
     old_contacts_list[index] = new_contact
     assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) ==\
         sorted(old_contacts_list, key=Contact.sorting_id_or_maxsize)
-    if check_ui:
-        assert sorted(new_contacts_list, key=Contact.sorting_id_or_maxsize) ==\
-            sorted(app.contact.get_contacts_list(), key=Contact.sorting_id_or_maxsize)
+
